26f19dc61dcb6fc8f9a884b138e89d93340dee28
Adeolu Amole <amoleadeoluadex@gmail.com>
2026-03-09 13:31:59 +0100

    feat: I added a description and favicon to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Payboost',
   description:
-    'Payboost combines digital payments with practical loan and savings solutions built for everyday users and small businesses. It also help you to Buy, sell, and manage your crypto assets with industry leading security. Seamlessly integrate your digital wallet with your daily spending.',
+    'Payboost combines digital payments with practical loan and savings solutions built for everyday users and small businesses. Payboost also help you to Buy, sell, and manage your crypto assets with industry leading security. Seamlessly integrate your digital wallet with your daily spending.',
 }
 
 export default function RootLayout({
